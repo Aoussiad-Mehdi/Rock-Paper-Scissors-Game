710c9232f5a7d16a10fd63dfbf720c77e88ac48c
Mehdi Aoussiad <mehdiouss315@gmail.com>
2021-06-16 23:49:43 +0100

    Update app.js
    Updating the game.

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-let computerScore = 1;
-let playerScore = 1;
+// Selecting elements.
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
 const showIcon = document.querySelector('.show i');
 const computerShowIcon = document.querySelector('.computer i');
 
-const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
+// Storing the scores.
+let computerScore = 1;
+let playerScore = 1;
+
+const rockIcon = "fas fa-hand-rock";
+const paperIcon = "fas fa-hand-paper";
+const scissorsIcon = "fas fa-hand-scissors";
+
+const randomClasses = [rockIcon, paperIcon, scissorsIcon];
 const text = document.getElementById('demo');
 const text2 = document.getElementById('demo2');
+
+const tie = ()=>{
+    text.innerHTML = "It's a Tie ! ";
+    text.style.color = 'orange';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'orange';
+}
+
+const win = ()=>{
+    text.innerHTML = "It's a Win ! ";
+    text.style.color = 'rgb(1, 146, 1)';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'rgb(1, 146, 1)';
+}
+
+const lose = ()=>{
+    text.innerHTML = "You Loosed ! ";
+    text.style.color = 'red';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'red';
+}
 
 // Game Functionality.
 const game = () =>{
@@ -19,61 +47,43 @@ const game = () =>{
            showIcon.className = clickedBtn;
            let randomNum = Math.floor(Math.random() * randomClasses.length);
            computerShowIcon.className = randomClasses[randomNum];
-           // Game Score.
+
            // If it's a Tie .
            if(showIcon.className === computerShowIcon.className){
                pScore.innerHTML = pScore.innerHTML;
                cScore.innerHTML = cScore.innerHTML;
-               text.innerHTML = "It's a Tie ! ";
-               text.style.color = 'orange';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'orange';
-           } // if it's not a Tie.
-           else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[2]){
+               tie();
+           }
+
+           // if it's not a Tie.
+           else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]){
+               win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]){
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]){
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]){
+               win();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]){
+               lose();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
+               win();
            }
         });
     });
 }
-// Calling the function.
+
+// Run the game.
 game();
